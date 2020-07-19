f7f1497c15b22b488ff94cc1c5a1f9ee5536c164
Your Name <shist95@mail.ru>
2020-07-19 23:06:05 +0800

    fix scripts

--- host_editor.sh
+++ host_editor.sh
@@ -3,12 +3,18 @@
 # ${ip[0]} = "12.0.0.1"
 # ${ip[1]} = "8.8.8.8"
 IFS=, read -a ip <<< $1
-if "${ip[0]}" = true ; then
-  ln -sf ./hostnet1acctive /etc/host
-	echo "Run server 12.0.0.1"
-fi
 
-if "${ip[1]}" = true ; then
-  ln -sf ./hostnet2acctive /etc/host
-	echo "Run server 8.8.8.8"
+if [[ "${ip[0]}" = 5 && "${ip[1]}" = 5 ]]; then
+  ln -sf ./hostnet1acctive /etc/host
+  echo "Run server 12.0.0.1"
+  else
+    if [[ "${ip[0]}" = 5 ]] ; then
+      ln -sf ./hostnet1acctive /etc/host
+      echo "Run server 12.0.0.1"
+    fi
+
+    if [[ "${ip[1]}" = 5 ]] ; then
+      ln -sf ./hostnet2acctive /etc/host
+      echo "Run server 8.8.8.8"
+    fi
 fi
--- replica_script.sh
+++ replica_script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 IFS=, read -a ip <<< $1
-if "${ip[0]}" = true && "${ip[1]}" = true; then
+if [[ "${ip[0]}" = 5 && "${ip[1]}" = 5 ]]; then
   ln -sf ./replica_backup.conf /opt/svyazcom/etc/replica.conf
 	echo "All servers run!"
   else
