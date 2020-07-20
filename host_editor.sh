61da7454c41f97b69eb1c2fe598962f51724c146
Your Name <shist95@mail.ru>
2020-07-20 09:47:31 +0800

    add run_ pinger script

--- host_editor.sh
+++ host_editor.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
-# $1 = ["12.0.0.1", "8.8.8.8"]
-# ${ip[0]} = "12.0.0.1"
+# ["77.88.8.8", "8.8.8.8"]
+# $1 = [5,5]
+# ${ip[0]} = "77.88.8.8"
 # ${ip[1]} = "8.8.8.8"
 IFS=, read -a ip <<< $1
 
 if [[ "${ip[0]}" = 5 && "${ip[1]}" = 5 ]]; then
   ln -sf ./hostnet1acctive /etc/host
-  echo "Run server 12.0.0.1"
+  echo "Run server 77.88.8.8"
   else
     if [[ "${ip[0]}" = 5 ]] ; then
       ln -sf ./hostnet1acctive /etc/host
-      echo "Run server 12.0.0.1"
+      echo "Run server 77.88.8.8"
     fi
 
     if [[ "${ip[1]}" = 5 ]] ; then
